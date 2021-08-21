769cc8570a0e1ab13a96519d280b365d52876d84
Hpro78891 <handsomeprogrammer753@gmail.com>
2021-08-21 15:53:48 +0530

    add navbar and home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,41 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu');
+        console.log('hello show me ');
+    });
+}
+
 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
+if(navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu');
+        console.log('click close');
+    });
+}
+
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 
 
 /*==================== ACCORDION SKILLS ====================*/
